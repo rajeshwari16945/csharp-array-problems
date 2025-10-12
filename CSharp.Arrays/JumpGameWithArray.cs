89a6a6ad722c0f671598da233dafcd8bcba9b250
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-10-11 20:21:40 -0500

    Jump game using arrays

---
+++ CSharp.Arrays/JumpGameWithArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class JumpGameWithArray
+    {
+        public void CanJump()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size =  int.Parse(Console.ReadLine());
+            int[] nums = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter element {i + 1}:");
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            int pos = 0;
+            while(pos < nums.Length)
+            {
+                if(pos == nums.Length - 1)
+                {
+                    Console.WriteLine("True");
+                    return;
+                } else
+                {
+                    pos += nums[pos];
+                }
+            }
+            Console.WriteLine("False");
+
+        }
+    }
+}
