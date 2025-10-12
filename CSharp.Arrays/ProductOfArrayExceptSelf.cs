ce3f78a76abeb5d69aa5ee3b9874ed482b7dffba
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-10-12 18:51:16 -0500

    multiplying the array of all elements except itself

---
+++ CSharp.Arrays/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class ProductOfArrayExceptSelf
+    {
+        public void getTheProductOfArrayExceptSelf()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] inputArray = new int[size];
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < size; i++)
+            {
+                inputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] resultArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                int result = 1;
+                for (int j = 0; j < size; j++)
+                {
+                    if(i != j)
+                    {
+                        result *= inputArray[j];
+                    }
+                }
+                resultArray[i] = result;
+            }
+
+            Console.WriteLine("The product of array except self is:" + string.Join(", ", resultArray));
+        }
+    }
+}
