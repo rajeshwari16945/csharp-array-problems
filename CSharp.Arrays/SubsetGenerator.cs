5216452d6590e443d0045e5a805a5aa3e916c56c
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-10-17 20:59:50 -0500

    class to generate the subset of the array

---
+++ CSharp.Arrays/SubsetGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class SubsetGenerator
+    {
+
+        public static List<List<int>> GenerateSubsets(List<int> nums)
+        {
+            var result = new List<List<int>>();
+            Generate(0, nums, new List<int>(), result);
+            return result;
+        }
+
+        private static void Generate(int index, List<int> nums, List<int> current, List<List<int>> result)
+        {
+            if (index == nums.Count)
+            {
+                result.Add(new List<int>(current));
+                return;
+            }
+
+            Generate(index + 1, nums, current, result);
+
+            current.Add(nums[index]);
+            Generate(index + 1, nums, current, result);
+            current.RemoveAt(current.Count - 1);
+        }
+    }
+}
