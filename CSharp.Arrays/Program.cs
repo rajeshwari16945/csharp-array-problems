96315a8facafacfb029450c179dbfb4e7afbbfb4
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-10-23 18:05:46 -0500

    program to implement stack algorithm using array

--- CSharp.Arrays/Program.cs
+++ CSharp.Arrays/Program.cs
@@ -22,8 +22,8 @@ class Program
         /*LongestSubArray longestSubArray = new LongestSubArray();
         longestSubArray.getLongestSubArray();*/
 
-        DynamicArray dynamicArray = new DynamicArray();
-        dynamicArray.Menu();
+        /*DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.Menu();*/
 
         //var nums = new List<int> { 1, 2, 3 };
         //var result = SubsetGenerator.GenerateSubsets(nums);
@@ -34,5 +34,14 @@ class Program
 
         /*var sol = new PermuteUnique();
         sol.GetPermuteUnique(new int[] { 1, 1, 2 });*/
+
+        StackImplementationWithArray stack = new StackImplementationWithArray();
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        stack.Push(40);
+        stack.Peek();
+        stack.Pop();
+        stack.Peek();
     }
 }
---
+++ CSharp.Arrays/StackImplementationWithArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class StackImplementationWithArray
+    {
+        int[] stackIntArray;
+        int count = 0;
+
+        public StackImplementationWithArray()
+        {
+            stackIntArray = new int[5];
+        }
+
+        public void Push(int value)
+        {
+            stackIntArray[count] = value;
+            count++;
+            PrintSack();
+        }
+
+        public void Pop()
+        {
+            if (count > 0)
+            {
+                stackIntArray[count - 1] = 0;
+                Console.WriteLine("Element popped successfully.");
+            }
+            count--;
+            PrintSack();
+        }
+
+        public void Peek()
+        {
+            if (count > 0)
+            {
+                Console.WriteLine("Top element is: " + stackIntArray[count - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty.");
+            }
+            PrintSack();
+        }
+
+        public void PrintSack()
+        {
+            for (int i = count - 1; i >= 0; i--)
+            {
+                Console.Write(stackIntArray[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
