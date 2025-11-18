e2ae8dae5f0f3b0d1d5df0b40300455f4cc4fc93
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-11-18 15:49:47 -0600

    This prgoram adjusts student grades by rounding them up to the next multiple of 5 when the difference is less than 3, unless the grade is below 38.

---
+++ CSharp.Arrays/StudentGrades.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class StudentGrades
+    {
+        public static void gradingStudents()
+        {
+            int[] grades = new int[] { 73, 67, 38, 33 };
+            int[] result = new int[grades.Length];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                int grade = grades[i];
+
+                if (grade < 38)
+                {
+                    result[i] = grade;
+                }
+                else
+                {
+                    int rounded = (int)(Math.Round(grade / 5.0) * 5);
+
+                    if (rounded < grade)
+                        result[i] = grade;
+                    else
+                        result[i] = rounded;
+                }
+            }
+
+            Console.WriteLine("Rounded Grades: " + string.Join(", ", result));
+        }
+
+    }
+}
