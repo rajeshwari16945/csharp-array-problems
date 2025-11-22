56831dbeb733989fb9ae1757cf7a0ffdbbac43f4
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-11-21 18:08:30 -0600

    program to find the length of the shortest subarray which, if sorted, will make the entire array sorted in non-decreasing order

---
+++ CSharp.Arrays/UnSortedSubArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class UnSortedSubArray
+    {
+        public void FindUnSortedSubArray()
+        {
+            int[] nums = {2, 6, 4, 8, 10, 9, 15};
+            int n = nums.Length;
+            int left = -1;
+            int right = -1;
+
+            int maxSeen = int.MinValue;
+            int minSeen = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                maxSeen = Math.Max(maxSeen, nums[i]);
+                if (nums[i] < maxSeen)
+                    right = i;
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                minSeen = Math.Min(minSeen, nums[i]);
+                if (nums[i] > minSeen)
+                    left = i;
+            }
+
+            if (left == -1) Console.WriteLine(0);
+
+            Console.WriteLine( right - left + 1);
+        }
+    }
+}
