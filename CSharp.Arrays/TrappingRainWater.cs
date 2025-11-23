93f39c5ab461243621ac27a0b1950d692ec06555
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-11-22 20:22:21 -0600

    program that finds how much rainwater can be trapped

---
+++ CSharp.Arrays/TrappingRainWater.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class TrappingRainWater
+    {
+        public static void Trap(int[] height)
+        {
+            if (height == null || height.Length == 0)
+                Console.WriteLine( 0);
+
+            int left = 0, right = height.Length - 1;
+            int leftMax = 0, rightMax = 0;
+            int trappedWater = 0;
+
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    if (height[left] >= leftMax)
+                        leftMax = height[left];
+                    else
+                        trappedWater += leftMax - height[left];
+
+                    left++;
+                }
+                else
+                {
+                    if (height[right] >= rightMax)
+                        rightMax = height[right];
+                    else
+                        trappedWater += rightMax - height[right];
+
+                    right--;
+                }
+            }
+
+            Console.WriteLine( trappedWater);
+        }
+
+    }
+}
