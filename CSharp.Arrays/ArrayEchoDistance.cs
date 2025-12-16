da20a009c0020e9af8410014c0cb16c424e4158d
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-12-16 12:41:42 -0600

    Implement Array Echo Distance to find nearest duplicate distances for each array element.

---
+++ CSharp.Arrays/ArrayEchoDistance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ArrayEchoDistance
+{
+
+    public static int[] GetArrayEchoDistance(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[n];
+        Array.Fill(result, -1);
+
+        Dictionary<int, int> lastIndex = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (lastIndex.ContainsKey(nums[i]))
+            {
+                int prev = lastIndex[nums[i]];
+                int distance = i - prev;
+                result[i] = distance;
+                if (result[prev] == -1 || distance < result[prev])
+                {
+                    result[prev] = distance;
+                }
+            }
+            lastIndex[nums[i]] = i;
+        }
+
+        return result;
+    }
+}
