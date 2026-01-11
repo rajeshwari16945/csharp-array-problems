8fcb05054e8ec574b0f4bc0532b4f00a5c695cfd
rajeshwari16945 <rajeshwari16945@gmail.com>
2026-01-11 09:42:24 -0600

    program to match deferred zeros with prior credits and future debits using stack and queue

---
+++ CSharp.Arrays/DeferredRebalancing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DeferredRebalancing
+{
+    public static int CountValidReconciliations(int[] events)
+    {
+        Stack<int> positiveStack = new Stack<int>(); 
+        Queue<int> zeroQueue = new Queue<int>();     
+        int validCount = 0;
+
+        foreach (int e in events)
+        {
+            if (e > 0)
+            {
+                positiveStack.Push(e);
+            }
+            else if (e == 0)
+            {
+                zeroQueue.Enqueue(0);
+            }
+            else 
+            {
+                if (zeroQueue.Count > 0 && positiveStack.Count > 0)
+                {
+                    zeroQueue.Dequeue();
+                    positiveStack.Pop();
+                    validCount++;
+                }
+            }
+        }
+
+        return validCount;
+    }
+
+    
+}
