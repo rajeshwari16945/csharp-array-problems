553d4810feddbe7bf678b18774648d696c08e2f1
rajeshwari16945 <rajeshwari16945@gmail.com>
2026-01-14 18:32:33 -0600

    C# solution for the Left–Right Difference Balance problem.

---
+++ CSharp.Arrays/LeftRightDifferenceBalance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class LeftRightDifferenceBalance
+    {
+        public static int MinBalanceIndex(int[] arr)
+        {
+            int n = arr.Length;
+
+            int totalSum = 0;
+            for (int i = 0; i < n; i++)
+                totalSum += arr[i];
+
+            int leftSum = 0;
+            int minBalance = int.MaxValue;
+            int resultIndex = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int rightSum = totalSum - leftSum - arr[i];
+                int balance = Math.Abs(leftSum - rightSum);
+
+                if (balance < minBalance)
+                {
+                    minBalance = balance;
+                    resultIndex = i;
+                }
+
+                leftSum += arr[i];
+            }
+
+            return resultIndex;
+        }
+    }
+}
