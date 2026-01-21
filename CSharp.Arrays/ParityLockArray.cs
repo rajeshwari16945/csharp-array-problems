ab77aaae82e654bfb2c54b146eacd79b98c59b60
rajeshwari16945 <rajeshwari16945@gmail.com>
2026-01-20 22:03:51 -0600

    program that checks whether all array elements can be made even using a parity invariant.

---
+++ CSharp.Arrays/ParityLockArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class ParityLockArray
+{
+    public static bool CanMakeAllEven(long[] arr)
+    {
+        int evenXor = 0, oddXor = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int parity = (int)(arr[i] & 1);
+            if ((i & 1) == 0)
+                evenXor ^= parity;
+            else
+                oddXor ^= parity;
+        }
+
+        return evenXor == oddXor;
+    }
+
+}
