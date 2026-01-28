2cef153d06f05f75b85f0d7a5321a70cda20e82a
rajeshwari16945 <rajeshwari16945@gmail.com>
2026-01-27 18:44:33 -0600

    influence logic so only zero-value elements are affected by propagation.

---
+++ CSharp.Arrays/ArrayInfluencePropagation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Arrays
+{
+    public class ArrayInfluencePropagation
+    {
+        public static int[] ApplyInfluence(int[] arr)
+        {
+            int n = arr.Length;
+            int[] diff = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = arr[i];
+                if (value == 0) continue;
+
+                if (value > 0)
+                {
+                    for (int j = i + 1; j <= i + value && j < n; j++)
+                    {
+                        if (arr[j] == 0)
+                            diff[j] += value;
+                    }
+                }
+                else
+                {
+                    for (int j = i - 1; j >= i + value && j >= 0; j--)
+                    {
+                        if (arr[j] == 0)
+                            diff[j] += value;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] += diff[i];
+            }
+
+            return arr;
+        }
+    }
+}
